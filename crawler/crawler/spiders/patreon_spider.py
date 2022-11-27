d24d30477be9750ffa272a590110493a10ebd24b
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-11-27 15:38:44 +0100

    Updated patreon spider to use the module

--- crawler/crawler/spiders/patreon_spider.py
+++ crawler/crawler/spiders/patreon_spider.py
@@ -1,11 +1,12 @@
 import scrapy
 from scrapy_playwright.page import PageMethod
+import artist_dict
 
 
 class PatreonSpider(scrapy.Spider):
     name = 'patreon'
     start_urls = [
-        'https://www.patreon.com/search?q=art', 
+        'https://www.patreon.com/search?q=art',
         'https://www.patreon.com/search?q=music',
         'https://www.patreon.com/search?q=photography',
         'https://www.patreon.com/search?q=writing',
@@ -20,7 +21,7 @@ class PatreonSpider(scrapy.Spider):
         'https://www.patreon.com/search?q=podcast',
         'https://www.patreon.com/search?q=stream',
         'https://www.patreon.com/search?q=video',
-        ]
+    ]
 
     custom_settings = {
         "TWISTED_REACTOR": "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
@@ -36,13 +37,12 @@ class PatreonSpider(scrapy.Spider):
         for url in self.start_urls:
             yield scrapy.Request(url, meta={
                 'playwright': True,
-                'playwright_include_page': True, 
+                'playwright_include_page': True,
                 'playwright_page_methods': [
                     PageMethod('wait_for_selector', 'div[data-tag="campaign-result"]')
-                    ],
+                ],
                 'errback': self.errback,
             })
-
 
     async def parse(self, response):
         """Parse the search results page to get the artist's name, short description, image, etc."""
@@ -58,11 +58,11 @@ class PatreonSpider(scrapy.Spider):
             posts = artist.css('p.sc-jrQzAO.DzYUV::text').get()
             patrons = artist.css('p[data-tag="campaign-result-patron-count"]::text').get()
             short_desc = artist.css('p.sc-jrQzAO.bsIqPC::text').get()
-            
+
             # parse posts and patrons as int
-            posts = int(posts[:-5].strip().replace(',','')) # remove 'posts'
-            if patrons: # patrons number might be missing (private)
-                patrons = int(patrons[:-7].strip().replace(',','')) # remove 'patrons'
+            posts = int(posts[:-5].strip().replace(',', ''))  # remove 'posts'
+            if patrons:  # patrons number might be missing (private)
+                patrons = int(patrons[:-7].strip().replace(',', ''))  # remove 'patrons'
 
             # follow the artist's page to get the long description, pricing, etc.
             yield scrapy.Request(url, callback=self.parse_artist, meta={
@@ -79,35 +79,55 @@ class PatreonSpider(scrapy.Spider):
         if next_page is not None:
             yield scrapy.Request(next_page, meta={
                 'playwright': True,
-                'playwright_include_page': True, 
+                'playwright_include_page': True,
                 'playwright_page_methods': [
                     PageMethod('wait_for_selector', 'div[data-tag="campaign-result"]')
-                    ],
+                ],
                 'errback': self.errback,
             })
-
 
     def parse_artist(self, response):
         """Parse the artist's page to get the long description, pricing, etc."""
         print('\033[90m' + "\tParsing: " + response.url + '\033[0m')
 
-        pricing = response.css('div.sc-bkkeKt.cupyBO::text').getall()
+        pricing = list(map(lambda tier:
+                           artist_dict.make_price_tier(tier.css('div.sc-bkkeKt.cupyBO::text').get(),
+                                                       tier.css('h3[data-tag="tier-title"]::text').get(),
+                                                       ' '.join(tier.css('.sc-1rlfkev-0.MtGiR *::text').getall()))
+                           , response.css('div[data-tag="reward-tier-card"]')))
         long_desc = response.css('div[data-tag="summary-container"] *::text').getall()
         long_desc = ' '.join(long_desc)
 
-        yield {
-            'name': response.meta['name'],
-            'posts': response.meta['posts'],
-            'patrons': response.meta['patrons'],
-            'pricing': pricing,
-            'url': response.url,
-            'short_desc': response.meta['short_desc'],
-            'image': response.meta['image'],
-            'long_desc': long_desc,
-            }
+        print(artist_dict.make(
+            self.name,
+            response.url,
+            response.meta['name'],
+            response.meta['image'],
+            response.meta['short_desc'],
+            long_desc,
+            response.meta['posts'],
+            response.meta['patrons'],
+            pricing,
+            [],
+            [],
+            ""
+        ))
 
+        yield artist_dict.make(
+            self.name,
+            response.meta['name'],
+            response.url,
+            response.meta['image'],
+            response.meta['short_desc'],
+            long_desc,
+            response.meta['posts'],
+            response.meta['patrons'],
+            pricing,
+            [],
+            [],
+            ""
+        )
 
     async def errback(self, failure):
         page = failure.request.meta["playwright_page"]
         await page.close()
-
