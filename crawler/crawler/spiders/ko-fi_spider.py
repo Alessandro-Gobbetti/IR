ce25b802c542004b3e66fc0187ff6fbf9a88cbed
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2022-11-27 15:50:54 +0100

    first kofi spider working version

---
+++ crawler/crawler/spiders/ko-fi_spider.py
@@ -0,0 +1,121 @@
+import scrapy
+from scrapy_playwright.page import PageMethod
+import artist_dict
+
+
+class KoFiSpider(scrapy.Spider):
+    name = 'ko-fi'
+
+    # kofitags = ['Advice', 'Animation', 'Art', 'Blogging',
+    #             'Comedy', 'Comics', 'Commissions', 'Community', 
+    #             'Cosplay', 'Crafts', 'Dance & Theatre', 'Design', 
+    #             'Drawing & Painting', 'Education', 'Food & Drink', 
+    #             'Fundraising', 'Game Development', 'Gaming', 
+    #             'Health & Fitness', 'Lifestyle', 'Money', 'Music', 
+    #             'News', 'Nsfw', 'Other', 'Photography', 'Podcast', 
+    #             'Science & Tech', 'Social', 'Software', 'Spirituality', 
+    #             'Streaming', 'Video and Film', 'Writing']
+
+    kofitags = ['Art']
+
+    # create an url for each tag
+    start_urls = [f'https://ko-fi.com/explore?category={tag}' for tag in kofitags ]
+
+    custom_settings = {
+        "TWISTED_REACTOR": "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
+        "DOWNLOAD_HANDLERS": {
+            "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+            "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+        },
+        "DOWNLOAD_DELAY": 2,
+        "LOG_LEVEL": "INFO",
+    }
+
+    def start_requests(self):
+        """Start the requests with playwright, so we can wait for the page to load."""
+        for url in self.start_urls:
+            yield scrapy.Request(url, meta={
+                'playwright': True,
+                'playwright_include_page': True,
+                # 'playwright_page_methods': [
+                #     PageMethod('wait_for_selector',
+                #                'div[data-tag="campaign-result"]')
+                # ],
+                'errback': self.errback,
+            })
+
+    async def parse(self, response):
+        """Parse the search results page to get the artist's name, short description, image, etc."""
+        page = response.meta['playwright_page']
+        await page.close()
+
+        print('\033[92m' + "Parsing: " + response.url + '\033[0m')
+
+        # get the artist's urls.
+        artist_urls = response.css('div.flex-wrap-explore-card a::attr(href)').getall()
+
+        for artist_url in artist_urls:
+            # the url is relative to the base url
+            artist_url = response.urljoin(artist_url)
+            # print(artist_url)
+
+            yield scrapy.Request(
+                artist_url, 
+                callback=self.parse_artist, 
+                meta={
+                    'playwright': True,
+                    'playwright_include_page': True,
+                    'errback': self.errback,
+                })
+
+
+    async def parse_artist(self, response):
+        """Parse the artist's page to get the long description, pricing, etc."""
+        page = response.meta['playwright_page']
+        await page.close()
+
+        print('\033[90m' + "\tParsing: " + response.url + '\033[0m')
+
+
+        name = response.css('div.kfds-text-limit-profilename-mobile span::text').get()
+        supporters = response.css('span.kfds-c-profile-link-handle.kfds-font-clr-dark::text').get()
+        # remove ' Supporters'
+        if supporters:
+            supporters = int(supporters[:-10].strip().replace(',', ''))
+        
+        pic = response.css('img#profilePicture::attr(src)').get()
+        banner_pic = response.css('div#profile-header::attr(style)').get()
+        # get the background:url(...) from the style attribute
+        banner_pic = banner_pic[banner_pic.find('background:url(')+len('background:url('):banner_pic.find(')')]
+        bio = response.css('p.kfds-c-para-control.line-breaks.break-long-words::text').get()
+        tags = response.css('span.label-tag::text').getall()
+        socials = response.css('div.social-profile-link a::attr(href)').getall()
+
+        # get the pricing
+        tiers = list(map(lambda tier:
+                         artist_dict.make_price_tier(
+                            amount_per_month= (tier.css('div.kfds-lyt-row.kfds-lyt-center span.kfds-font-size-16.kfds-font-bold::text').get() +
+                                               tier.css('div.kfds-lyt-row.kfds-lyt-center span.kfds-font-size-20.kfds-font-bold::text').get()),
+                            title=tier.css('.kfds-font-bold.kfds-lyt-width-100.kfds-font-size-20.kfds-c-word-wrap.text-center::text').get(),
+                            description=tier.css('div.ds-benefit-wrapper p.break-long-words.kfds-c-word-wrap.line-breaks::text').get()),
+            response.css('div.ds-tier-flex')))
+
+
+        yield artist_dict.make(
+            site=self.name,
+            page_link=response.url,
+            artist_name = name,
+            artist_image = pic,
+            artist_banner = banner_pic,
+            bio=bio,
+            bio_long=None,
+            amount_post=None,
+            amount_subs=supporters,
+            price_tiers=tiers, 
+            tags=tags, 
+            socialmedias=socials,
+        )
+
+    async def errback(self, failure):
+        page = failure.request.meta["playwright_page"]
+        await page.close()
