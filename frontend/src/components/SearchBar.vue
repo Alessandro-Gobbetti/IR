ca2aee16741c880d9c78f9a59da02ff53b0e42ed
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2022-11-30 18:31:53 +0100

    added search bar component

---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- input text -->
+  <input
+    type="text"
+    class="form-control"
+    placeholder="Search"
+  />
+</template>
+
+<script>
+// Components
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SearchBar",
+
+  components: {
+    // HelloWorld,
+  },
+});
+</script>
+
+
+<style scoped>
+.form-control {
+  width: 50%;
+  margin-left: 25%;
+  border-radius: 10vh;
+  padding: 10px 20px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+}
+
+.form-control:focus { /* TODO: add suggestions */
+  border: none;
+  outline: none;
+}
+</style>
