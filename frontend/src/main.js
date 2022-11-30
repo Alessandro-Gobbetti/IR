1807a498347da6668b499d3d4ee8ec05354418f1
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2022-11-30 18:33:01 +0100

    first ui implementation

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import VueSnip from 'vue-snip'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
@@ -11,4 +12,5 @@ createApp(App)
   .use(router)
   .use(store)
   .use(vuetify)
+  .use(VueSnip)
   .mount('#app')
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,45 @@
 <template>
-  <HelloWorld />
+  <div>
+    <SearchBar />
+
+    <ArtistCardBig />
+    <ArtistCard />
+    <ArtistCard />
+    <ArtistCard />
+    <ArtistCard />
+    <ArtistCard />
+    <ArtistCard />
+    <ArtistCard />
+    <ArtistCard />
+    <ArtistCard />
+    <ArtistCard />
+    <ArtistCard />
+    <ArtistCard />
+    <ArtistCard />
+
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import SearchBar from "@/components/SearchBar.vue";
+import ArtistCardBig from "@/components/ArtistCardBig.vue";
+import ArtistCard from "@/components/ArtistCard.vue";
 
 export default defineComponent({
-  name: 'HomeView',
+  name: "HomeView",
 
   components: {
-    HelloWorld,
+    SearchBar,
+    ArtistCard,
+    ArtistCardBig,
   },
+
+  data: () => ({
+    menu: false
+  })
 });
 </script>
+
