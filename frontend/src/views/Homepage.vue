c00c5d4d81197a0f522d36e07edcbaa91b001b6d
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-12-03 12:25:50 +0100

    Committed missing pages

---
+++ frontend/src/views/Homepage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="h-100 w-100 d-flex flex-column justify-center align-center">
+    <img class="rounded-lg" src="@/assets/images/rambo.gif" />
+    <SearchBar :onchange="onchange" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+// Components
+import SearchBar from "@/components/SearchBar.vue";
+import router from "@/router";
+
+export default defineComponent({
+  name: "Homepage",
+
+  components: {
+    SearchBar
+  },
+
+  methods: {
+    async onchange(value) {
+      if (!value)
+        return
+
+      await router.push(`/search?q=${value}`)
+      router.forward()
+    }
+  }
+});
+</script>
+
---
+++ frontend/src/views/ResultsPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="h-100 w-100 d-flex flex-column align-center">
+    <SearchBar :initial-value="this.$route.query.q" :fetch-initial-value="true" :onchange="onchange" :onresultschange="onresultschange" />
+
+    <SearchResults :results="results"/>
+
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+// Components
+import SearchBar from "@/components/SearchBar.vue";
+import ArtistCardBig from "@/components/ArtistCardBig.vue";
+import ArtistCard from "@/components/ArtistCard.vue";
+import SearchResults from "@/components/SearchResults.vue";
+import store from "@/store";
+import router from "@/router";
+
+export default defineComponent({
+  name: "ResultsPage",
+
+  components: {
+    SearchResults,
+    SearchBar,
+    ArtistCard,
+    ArtistCardBig,
+  },
+
+  data: () => ({
+    results: [],
+  }),
+
+  methods: {
+    onchange(value) {
+      router.push(`/search?q=${value}`)
+      router.forward();
+    },
+    onresultschange(result) {
+      this.results = result
+    }
+  }
+});
+</script>
+
